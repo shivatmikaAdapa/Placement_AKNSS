6483026f4be33adf0be6c9ef5802fad1083b547a
shivatmikaAdapa <131965075+shivatmikaAdapa@users.noreply.github.com>
2024-02-04 19:32:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function dashboard(){
+
+    window.location.href = "./dashboard.html";
+}
+
+function program(){
+
+    window.location.href = "./programming.html";
+}
+
+function aptitude(){
+
+    window.location.href = "./aptitude.html";
+
+}
+
+function dsa(){
+
+    window.location.href = "./dsa.html";
+
+}
+
+function csf(){
+
+    window.location.href = "./csf.html";
+
+}
+
+function web(){
+
+    window.location.href = "./web.html";
+
+}
+
+function sql(){
+
+    window.location.href = "./sql.html";
+
+}
+
+function oops(){
+
+    window.location.href = "./oops.html";
+    
+}
